e60873302a5582c927d55abd86e79f3ce9f7598f
Nikita <chalykh@artw.ru>
2024-03-08 11:23:52 +0300

    Отредактированы настройки проекта

--- deposit_calculator/config/settings.py
+++ deposit_calculator/config/settings.py
@@ -12,7 +12,7 @@ class ApplicationSettings(AdvancedSettings):
 
 
 class SiteSettings(AdvancedSettings):
-    host: str = Field("0.0.0.0", alias="SITE_HOST")
+    host: str = Field("127.0.0.1", alias="SITE_HOST")
     port: int = Field(8000, alias="SITE_PORT")
     log_level: str = Field("info", alias="SITE_LOG_LEVEL")
     reload: bool = Field(True, alias="SITE_RELOAD")
