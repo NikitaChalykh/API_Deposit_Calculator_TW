06975df25ac7f5df507aeaf49fcdd1125e2e951f
Nikita Chalykh <Wizardus@list.ru>
2025-03-04 12:53:30 +0300

    Update base.py

--- deposit_calculator/config/base.py
+++ deposit_calculator/config/base.py
@@ -2,4 +2,4 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class AdvancedSettings(BaseSettings):
-    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8', extra='ignore')
+    model_config = SettingsConfigDict(extra='ignore')
